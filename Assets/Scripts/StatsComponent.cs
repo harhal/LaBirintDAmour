fb6f46784ed2fe2758af9cfd01e517d303d43552
Konklaw <konklaw@gmail.com>
2018-07-14 09:41:30 +0300

    added: hp hud

--- Assets/Scripts/StatsComponent.cs
+++ Assets/Scripts/StatsComponent.cs
@@ -2,15 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatsComponent : MonoBehaviour
 {
 	public const float MaxHealth = 150;
+	public Text text;
 	[SerializeField]
 	private float Health = 100;
+
+	private void Start()
+	{
+		Validate();
+	}
 
 	public void ChangeHealth(float deltaHealth)
 	{
 		Health += deltaHealth;
+		Validate();
+	}
+
+	public void Validate()
+	{
+		text.text = Health.ToString();
 	}
 }
