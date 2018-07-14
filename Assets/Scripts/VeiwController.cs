a7d2716e36730efe7dc1403405e57ff54c5d2bc1
Konklaw <konklaw@gmail.com>
2018-07-14 10:21:34 +0300

    added: veiw controller

---
+++ Assets/Scripts/VeiwController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeiwController : MonoBehaviour {
+
+	Transform[] characters;
+
+	void Start () {
+		var components = FindObjectsOfType<InputComponent>();
+		characters = new Transform[components.Length];
+		for (int i = 0; i < characters.Length; i++)
+		{
+			characters[i] = components[i].gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 position = new Vector3();
+		for (int i = 0; i < characters.Length; i++)
+		{
+			position += characters[i].position;
+		}
+		position /= characters.Length;
+		Vector3 cameraPosition = transform.position;
+		cameraPosition.y = position.y;
+		transform.position = cameraPosition;
+	}
+}
