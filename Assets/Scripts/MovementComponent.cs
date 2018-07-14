a6721dae4e5cdb72968330c40bf15cc46a9a77b6
harhal19 <harhal19@mail.ru>
2018-07-14 11:58:06 +0300

    Mummy AI movement

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovementComponent : MonoBehaviour {
 
     private Rigidbody2D Body;
