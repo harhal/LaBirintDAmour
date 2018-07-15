5921f34187eb02a1c1863ec272fb11a18b16f408
Konklaw <konklaw@gmail.com>
2018-07-15 11:46:12 +0300

    edede: happy end image

--- Assets/EndGameScript.cs
+++ Assets/EndGameScript.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGameScript : MonoBehaviour {
 
+	public Image image;
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			SceneManager.LoadScene("Menu");
+			//Application.Quit();
+		}
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		Debug.Log("Extis");
+		Time.timeScale = 0;
+		image.gameObject.SetActive(true);
+		//Debug.Log("Extis");
 	}
 }
